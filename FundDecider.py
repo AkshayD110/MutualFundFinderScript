52f8440ece40809223611b6fd0f17d077438c0d4
AkshayD110 <akshayd110@gmail.com>
2018-01-17 05:26:23 +0530

    updated With main function call

--- FundDecider.py
+++ FundDecider.py
@@ -20,7 +20,7 @@ def main(path):
         """gets the number of columns and rows in the sheet"""
         noColms=currentSheet.ncols
         noRows=currentSheet.nrows
-        
+
         #print noRows, noColms
 
         """iterates through all the columns and appending  the values"""
@@ -33,18 +33,6 @@ def main(path):
         iterator=iterator+1
         #print (fundBook.sheet_by_index(iterator).nrows)
 
-    #print (len(listOfFunds))
-    #print listOfFunds
-    #print (len(listOfFunds))
-    #print listOfFunds
-    """below loop not required, as it is taken care of in the above loop"""
-    """this listOfFunds list has unicodes in its values. Iterate through it, and convert all values to str, and add to another list"""
-    #for allFunds in listOfFunds:
-        #print allFunds
-     #   thisFund=allFunds
-      #  thisFund=str(thisFund) #important to convert it to str from cell type to do collection.counter operation next
-       # thisFund=thisFund[6:] #slicing the data to remove the unicode values
-        #look more in to how slicing works
         #https://stackoverflow.com/questions/509211/understanding-pythons-slice-notation
         #finalListOfFunds.append(thisFund) #final list which will have all the fund names in string format
 

d63c2359bc4089bb18a145ea668487a9efca5d8b
Akshay <akshayd110@gmail.com>
2018-01-16 22:31:44 +0530

    Delete updateExcelfile.py

--- updateExcelfile.py
+++
@@ -1,9 +0,0 @@
-from xlrd import open_workbook
-from
-
-def editAnExcelFile(path):
-    openBook=open_workbook(path)
-    wb=
-
-
-editAnExcelFile(r"C:\Users\akshdesh.ORADEV\Documents\books\python\newBook.xlsx")
